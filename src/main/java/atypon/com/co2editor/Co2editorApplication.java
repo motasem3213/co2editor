868f439b720e7871b890a72fe954c31fe71c3e10
motasem3213 <moatasemammoura7@gmail.com>
2024-09-29 21:15:31 +0300

    first commit

---
+++ src/main/java/atypon/com/co2editor/Co2editorApplication.java
@@ -0,0 +1,13 @@
+package atypon.com.co2editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2editorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2editorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atypon/com/co2editor/Co2editorApplicationTests.java
@@ -0,0 +1,13 @@
+package atypon.com.co2editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2editorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
